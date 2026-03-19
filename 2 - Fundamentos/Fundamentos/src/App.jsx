77718f8658ff858aa8ccb11e8e0f517516ba8f74
Juliana Nascimento dos Santos <juliana_jns_2010@hotmail.com>
2026-03-19 12:02:12 -0300

    React - Atividades - 19/03/2026
    React - Atividades - 19/03/2026

--- 2 - Fundamentos/Fundamentos/src/App.jsx
+++ 2 - Fundamentos/Fundamentos/src/App.jsx
@@ -2,16 +2,60 @@ import './App.css'
 import FirstComponents from './components/FirstComponents'
 import MyComponent from './components/MyComponent'
 import TemplateExpressions from './components/TemplateExpressions'
+import Events from './components/Events'
+
 
 
 function App() {
 
   return (
     <>
-      {/* Importando compontentes*/}
+      {/*Importando compontentes
       <MyComponent />
       <FirstComponents />
       <TemplateExpressions />
+      <Events />*/
+      }
+
+      <div>
+
+        <h1>Pokemon de Água</h1>
+        <hr />
+        <br />
+
+        <table border="1" >
+          <tr>
+            <th>Pokemon</th>
+            <th>Imagem</th>
+            <th>Tipo</th>
+          </tr>
+
+          <tr>
+            <td>Squirtle</td>
+            <td> <img src="Squirtle2.jpg" alt="Squirtle" width="300"/>
+            </td>
+            <td>Água</td>
+          </tr>
+
+          <tr>
+            <td>Lapras</td>
+            <td>  <img src="Lapras2.jpg" alt="Lapras" width="300"/>
+            </td>
+            <td>Água</td>
+          </tr>
+
+
+          <tr>
+            <td>Psyduck</td>
+            <td>  <img src="Psyduck.png" alt="Psyduck" width="300"/>
+            </td>
+            <td>Água</td>
+          </tr>
+         </table>
+      </div>
+
+
+
 
     </>
   )
--- 2 - Fundamentos/Fundamentos/src/components/Events.jsx
+++ 2 - Fundamentos/Fundamentos/src/components/Events.jsx
@@ -1,8 +1,54 @@
-const Events = () =>{
-const handleMyEvent = (e) =>{
-    console.log(e);
-    console.log();
-}
+const Events = () => {
 
 
+    const handleMyEvent = (e) => {
+        console.log(e);
+        console.log("Evento 1 ativado");
+    };
+    const renderSomething = (x) => {
+        if (x) {
+            return <h2>Renderizando True!</h2>
+        } else {
+            return <h2>Renderizando False!</h2>
+        }
+    };
+    const somaValores = () => {
+        console.log("Soma de 10 + 20 = " + (20 + 10))
+    }
+
+    const somaValores2 = (x, y) => {
+        
+            alert("A soma dos valores: " + (x+y));
+
+
+    }
+
+    return (
+        <div>
+            <div>
+                <button onClick={handleMyEvent}>Função Maior com Nome!</button>
+            </div>
+            <div>
+                <button onClick={() => console.log("Evento 2 ativado!")}>Função Menor sem Nome! </button>
+            </div>
+            <div>
+                {renderSomething(true)}
+                {renderSomething(false)}
+            </div>
+            <div>
+                <button onClick={somaValores}>Soma no console</button>
+            </div>
+
+            <div>
+            <button onClick={() => somaValores2(20,10)}>
+  Soma no alert
+</button>
+            </div>
+
+
+        </div>
+    )
+
 }
+
+export default Events;
