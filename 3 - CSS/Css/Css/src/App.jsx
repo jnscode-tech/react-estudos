081d4e9a5160fc7bfe2c64123e37f1f4b8d133e9
Juliana Nascimento dos Santos <juliana_jns_2010@hotmail.com>
2026-03-23 11:23:50 -0300

    CSS - React
    Css - React

---
+++ 3 - CSS/Css/Css/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import './App.css'
+import Exemplo from './Components/Exemplo.jsx'
+import ExemploSemCss from './Components/ExemploSemCss.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  //CSS Global: index.css (Vermelho)
+  //CSS Local: App.css (Mais forte que o global) (Azul)
+  //CSS Local: Exemplo.Css se não criarmos ele acaba pegando o local do App.css ou o Global.css (Verde) Cuidado pois este pode acabar vazando para outros;
+  //CSS Module: Exemplo.module.css (Tem que ser importado no seu componente como um objeto e usar className={estilos.nomeDaClasse}) evita vazar para outros componentes.
+
+  let autor = 'Marcos'
+
+  return (
+    <>
+      {/* Exemplo sem Css pegando o CSS Exemplo.css erroneamente porque quando colocamos uma configuração global em algum css componente ele aplica em todos, temos que usar o CSS Module e className.
+      Ex: Exemplo.module.css + Exemplo.jsx (parte comentada) */}
+      <ExemploSemCss />
+
+      {/* CSS Local App.css ou CSS Global Index.css */}
+      <h1>Formatação Local / Global</h1>
+
+      {/* CSS Local Exemplo.css */}
+      <Exemplo />
+
+      {/* Formatação com classe do App.css*/}
+      <h1 className='titulo'>Formatação com Classe</h1>
+
+      {/*Formatação com ID do App.css*/}
+      <h1 id='meuid'>Formatação com ID</h1>
+
+      {/* -------------------------------------------------- */}
+      
+      {/* CSS Inline */}
+        <p style={{ color: 'blue', borderTop: '2px solid blue', display: 'inline-block' }}>Formatação com CSS Inline</p>
+
+      {/* CSS Inline Dinâmico */}
+        <p style={{ color: autor === 'Marcos' ? 'orange' : 'blue' }}>Formatação com CSS Inline Dinâmico</p>
+
+        <p style={{ 
+                    color: autor === 'Marcos' ? 'orange' : 'blue',          
+          backgroundColor: autor === 'Marcos' ? 'lightblue' : 'lightgray' }}>Formatação com CSS Inline Dinâmico 2</p>
+
+      {/* Classes Dinâmicas */}
+        <h1 className={autor === 'Marcos' ? 'marcos' : 'titulo'}>Formatação com Classe Dinâmica</h1>
+
+    </>
+  )
+}
+
+export default App
---
+++ 3 - CSS/Css/Css/src/Components/Exemplo.jsx
@@ -0,0 +1,11 @@
+import './Exemplo.css'
+// import styles from './Exemplo.module.css' <------ Dessa forma utilizando o Module os estilos não vazariam para os outros componentes.
+
+const Exemplo = () => {
+    return(
+        <div>
+        <h1>Exemplo de Componente</h1><hr />
+    </div>
+    )
+}
+export default Exemplo
